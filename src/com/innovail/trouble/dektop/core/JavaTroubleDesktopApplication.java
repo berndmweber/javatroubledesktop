fbd98e7fbb7a8da49ffca2062b3e9071397bf917
Bernd Michael Weber <berndmicweber@innovail.com>
2012-02-26 15:16:59 -0800

    Adding application code.

---
+++ src/com/innovail/trouble/dektop/core/JavaTroubleDesktopApplication.java
@@ -0,0 +1,23 @@
+/**
+ * @file:   com.innovail.trouble.core - JavaTroubleDesktopApplication.java
+ * @date:   Feb 26, 2012
+ * @author: bweber
+ */
+package com.innovail.trouble.dektop.core;
+
+import com.badlogic.gdx.backends.jogl.JoglApplication;
+import com.innovail.trouble.core.JavaTroubleApplication;
+
+/**
+ * 
+ */
+public class JavaTroubleDesktopApplication {
+
+    /**
+     * @param args
+     */
+    public static void main (String[] args) {
+        new JoglApplication(new JavaTroubleApplication(), "Trouble", 640, 480, false);
+    }
+
+}
